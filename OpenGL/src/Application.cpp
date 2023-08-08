4bf14a4666626f34599fe69e2c42562be1833ccf
neamesis <mastershooter64@gmail.com>
2023-08-09 00:18:50 +0530

    3D camera movement (1)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -25,6 +25,27 @@ void process_input(GLFWwindow* window)
 }
 
 
+void camera_move(GLFWwindow* window, glm::vec3& cameraPos, glm::vec3& cameraFront, glm::vec3& cameraUp, float cameraSpeed)
+{
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		cameraPos += cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+}
+
+
 int main(int argc, char** argv)
 {
 	glfwInit();
@@ -190,7 +211,7 @@ int main(int argc, char** argv)
 	}
 	stbi_image_free(data1);
 
-	float transparent_factor = 0.25;
+	float transparent_factor = 0.35;
 
 	ShaderObject.use();
 	glUniform1i(glGetUniformLocation(ShaderObject.ID, "texture1"), 0);
@@ -203,11 +224,11 @@ int main(int argc, char** argv)
 
 	glm::vec3 cubePositions[10] = {
 		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(2.0f,  4.0f, -1.50f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(-3.8f, -2.0f, -1.23f),
+		glm::vec3(2.4f, -0.4f, -3.0f),
+		glm::vec3(-1.7f,  3.0f, -1.75f),
 		glm::vec3(1.3f, -2.0f, -2.5f),
 		glm::vec3(1.5f,  2.0f, -2.5f),
 		glm::vec3(1.5f,  0.2f, -1.5f),
@@ -216,10 +237,18 @@ int main(int argc, char** argv)
 
 	glEnable(GL_DEPTH_TEST);
 
-	float move_var = 0.01f;
-	float camera_x = 0.0f;
-	float camera_y = 0.0f;
-	float camera_z = -4.0f;
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+	glm::vec3 worldSpaceUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 cameraRight = glm::normalize(glm::cross(worldSpaceUp, cameraDirection));
+	glm::vec3 cameraUp = glm::normalize(glm::cross(cameraDirection, cameraRight));
+
+	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	float cameraSpeed = 0.35;
 
 	while (!glfwWindowShouldClose(window))
 	{	
@@ -251,25 +280,10 @@ int main(int argc, char** argv)
 		model = glm::mat4(1.0f);
 		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-		{
-			camera_x += move_var;
-		}
-		else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		{
-			camera_x -= move_var;
-		}
-		else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		{
-			camera_y -= move_var;
-		}
-		else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		{
-			camera_y += move_var;
-		}
+		camera_move(window, cameraPos, cameraFront, cameraUp, cameraSpeed);
 
 		view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(camera_x, camera_y, -4.0f));
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, worldSpaceUp);
 
 		projection = glm::mat4(1.0f);
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
@@ -291,11 +305,7 @@ int main(int argc, char** argv)
 			float angle = 20.0f * i;
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			glm::mat4 final_matrix = projection * view * model;
-
-			if (i % 3 == 0)
-			{
-
-			}
+			
 
 			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(final_matrix));
 
--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (i % 3 == 0)
+		{
+			std::cout << i << '\n';
+		}
+	}
+}
